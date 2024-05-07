ac29e348f2fe00a66ff071ccaf15000f08a4d0d5
usmonzodasomon <budushiyprogrammist@gmail.com>
2024-05-07 15:56:16 +0500

    add: config

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/usmonzodasomon/shortener/internal/config"
+)
+
 func main() {
-	
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"time"
@@ -10,15 +11,13 @@ import (
 type Config struct {
 	Env        string `yaml:"env" env-default:"local"`
 	HTTPServer `yaml:"http_server"`
-	Database   `yaml:"database"`
+	Database
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
-	User        string        `yaml:"user" env-required:"true"`
-	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 type Database struct {
@@ -30,6 +29,9 @@ type Database struct {
 }
 
 func MustLoad() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("cannot load env: %s", err)
+	}
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
